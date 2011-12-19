84590dd29ed72709d2bd2cf9343d7bcc6e32b835
Anthony Mastrean <anthony.mastrean@gmail.com>
2011-12-19 12:31:35 -0500

    trying to build this gdf thing

--- minecraft/tools/chocolateyInstall.ps1
+++ minecraft/tools/chocolateyInstall.ps1
@@ -1,55 +1,21 @@
-# Oh, this is a giant pain in the ass. How do I get a COM interface instance?
-# http://msdn.microsoft.com/en-us/library/windows/desktop/hh437965(v=VS.85).aspx
-
-function Create-Shortcut
-{
-    $shell = New-Object -ComObject 'Wscript.Shell'
-    $shortcut = $shell.CreateShortcut($shortcutPath)
-    $shortcut.TargetPath = $targetPath  
-    $shortcut.WorkingDirectory = $content
-    #$shortcut.IconLocation = $iconPath
-    $shortcut.Save()
-}
-
 try 
 { 
     $name    = 'minecraft'
+    $webfile = 'https://s3.amazonaws.com/MinecraftDownload/launcher/Minecraft.exe'
+    
     $tools   = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
-    
     $content = Join-Path (Split-path $tools) 'content'
-    New-Item $content -Type Directory
 
-    Get-ChocolateyWebFile "$name" "$content\Minecraft.exe" 'https://s3.amazonaws.com/MinecraftDownload/launcher/Minecraft.exe'
+    Get-ChocolateyWebFile $name "$content\Minecraft.exe" $webfile
 
-    $guid = [guid]::NewGuid()
+    .\Install-Game.ps1 -binaryPath "$tools\Minecraft.dll" `
+                       -installDirectory "$content" `
+                       -installScope 3 #AllUsers
     
-    # Create the special Game Explorer folders
-    $gamesEx = "$ENV:LOCALAPPDATA\Microsoft\Windows\GameExplorer"
-    $gameDir = "$gamesEx\{$guid}\PlayTasks\0"
-    New-Item "$gameDir" -Type Directory
-
-    # Create the Play shortcut in the new folder
-    $targetPath   = Join-Path $content 'Minecraft.exe'
-    $shortcutPath = Join-Path $gameDir 'Play.lnk'
-    Create-Shortcut
-    
-    # Add the necessary registry key/values
-    # The reg key on my desktop is: 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\GameUX\S-1-5-21-3693514532-2603672733-3282153189-1001'
-    #
-    $gamesExReg = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\GameUX\S-1-5-21-1069610301-324996817-3984272044-1000"
-    $gameReg    = "$gamesExReg\{$guid}"
-    New-Item "$gameReg"
-    
-    New-ItemProperty $gameReg -name "AppExePath"            -value "$shortcutPath"
-    New-ItemProperty $gameReg -name "ApplicationId"         -value "{$guid}"
-    New-ItemProperty $gameReg -name "ConfigApplicationPath" -value "$content"
-    New-ItemProperty $gameReg -name "ConfigInstallType"     -value "4"
-    New-ItemProperty $gameReg -name "Title"                 -value "Minecraft"
-    
-    Write-ChocolateySuccess "$name"
+    Write-ChocolateySuccess $name
 } 
 catch 
 {
-    Write-ChocolateyFailure "$name" "$($_.Exception.Message)"
+    Write-ChocolateyFailure $name "$($_.Exception.Message)"
     throw 
 }
