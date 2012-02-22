2cfbb2c0724b781143a9c3491b4ac9c24f305424
Anthony Mastrean <anthony.mastrean@gmail.com>
2012-02-22 00:36:40 -0500

    adding the rest of the native files

--- minecraft/tools/chocolateyInstall.ps1
+++ minecraft/tools/chocolateyInstall.ps1
@@ -7,12 +7,41 @@ try
     $content = Join-Path (Split-Path $tools) 'content'
     $exepath = Join-Path $content 'Minecraft.exe'
 
-    Get-ChocolateyUrl $name $exepath $url
+    #Get-ChocolateyUrl $name $exepath $url
     
-    Write-ChocolateySuccess $name
+    $binaryPath       = Join-Path $content 'Minecraft.dll'
+    $installDirectory = $content
+    $installScope     = 3 # AllUsers
+    
+    # HACK so DllImport can find the Gameux dll
+    $ENV:PATH += ";$tools"
+    
+    $signature = @"
+[DllImport("GameuxInstallHelper.dll", CharSet = CharSet.Auto)]
+public static extern int GameExplorerInstall(
+    [MarshalAs(UnmanagedType.LPTStr)]string binaryPath,
+    [MarshalAs(UnmanagedType.LPTStr)]string installDirectory,
+    int installScope);
+"@
+    
+    $type = Add-Type -Name "InstallGame" `
+                     -Namespace "GameUx" `
+                     -MemberDefinition $signature `
+                     -PassThru
+    
+    $result = $type::GameExplorerInstall($binaryPath, $installDirectory, $installScope)
+    
+    $ns   = 'root\cimv2\applications\games'
+    $game = Get-WmiObject Game -Namespace $ns | ?{ $_.Name -eq 'Minecraft' }
+                
+    if(-not $game) { 
+        throw "Minecraft was not installed correctly. Result: $result"
+    }
+    
+    #Write-ChocolateySuccess $name
 } 
 catch 
 {
-    Write-ChocolateyFailure $name $($_.Exception.Message)
+    #Write-ChocolateyFailure $name $($_.Exception.Message)
     throw 
 }
